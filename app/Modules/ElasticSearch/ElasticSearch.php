b634d12b39b5b39463fd16abf48d389da7264a31
farrukhjon <nazfar1988@gmail.com>
2024-04-12 17:07:00 +0500

    elasticsearch

---
+++ app/Modules/ElasticSearch/ElasticSearch.php
@@ -0,0 +1,435 @@
+<?php
+
+namespace App\Modules\ElasticSearch;
+
+use App\Modules\Telegram\Telegram;
+use GuzzleHttp\Client as Guzzle;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Log;
+
+class ElasticSearch
+{
+    private string $hostPort;
+
+    private Guzzle $guzzle;
+
+    private readonly array $headers;
+
+    private array $result;
+
+    private int $requestStatus;
+
+    public function __construct(
+        private readonly string $searchField,
+        private readonly string $indexName,
+        private readonly string $documentName,
+        private readonly array  $indexSettings,
+        private readonly string $initials,
+    ) {
+        $host = config('database.connections.elasticsearch.hosts');
+        $this->hostPort = $host['scheme'] .
+            '://' .
+            $host['host'] . ':' .
+            $host['port'];
+        $this->guzzle = new Guzzle();
+        $this->headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json'
+        ];
+    }
+
+    /**
+     *      "settings": {
+     *          "analysis": {
+     *             "tokenizer": {
+     *                  "trigram_tokenizer": {
+     *                      "type": "ngram",
+     *                      "min_gram": 3,
+     *                      "max_gram": 3
+     *                  }
+     *              },
+     *              "analyzer": {
+     *                  "trigram_analyzer": {
+     *                      "type": "custom",
+     *                      "tokenizer": "trigram_tokenizer"
+     *                  }
+     *              }
+     *          }
+     *      },
+     *      "mappings": {
+     *          "properties": {
+     *              "content": {
+     *                  "type": "text",
+     *                  "analyzer": "trigram_analyzer"
+     *              }
+     *          }
+     *      }
+     *
+     * ==============
+     * PUT /my-index-000001
+     * {
+     * "settings": {
+     * "index": {
+     * "number_of_shards": 3,
+     * "number_of_replicas": 2
+     * }
+     * }
+     * }
+     *
+     * @throws GuzzleException
+     */
+    public function createIndex(): void
+    {
+        $this->guzzle->put(
+            "{$this->hostPort}/{$this->indexName}",
+            [
+                'headers' => $this->headers,
+                'json' => $this->indexSettings
+            ]
+        );
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function health(): array
+    {
+        $response = $this->guzzle->get("{$this->hostPort}", [
+            'headers' => $this->headers,
+        ]);
+
+        return [$response->getBody()->getContents(), 200];
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function qQuery(string $uriSuffix): void
+    {
+        $response = $this->guzzle->get(
+            "{$this->hostPort}/{$uriSuffix}",
+            [
+                'headers' => $this->headers,
+            ]
+        );
+
+        $this->result = [$response->getBody()->getContents()];
+    }
+
+    /**
+     * POST /my_index/_search
+     * {
+     *      "query": {
+     *          "fuzzy": {
+     *              "content": {
+     *                  "value": "exaample",
+     *                  "fuzziness": 2
+     *              }
+     *          }
+     *      }
+     * }
+     *
+     * ============
+     * curl -X GET "localhost:9200/_search?pretty" -H 'Content-Type: application/json' -d'
+     * {
+     * "query": {
+     * "fuzzy": {
+     * "user.id": {
+     * "value": "ki"
+     * }
+     * }
+     * }
+     * }
+     * '
+     *
+     * @throws GuzzleException
+     */
+    public function fuzzySearch(): void
+    {
+        $response = $this->guzzle->post(
+            "{$this->hostPort}/{$this->indexName}/_search",
+            [
+                'headers' => $this->headers,
+                'json' => [
+                    "query" => [
+                        "match" => [
+                            "$this->documentName" => [
+                                "query" => $this->initials,
+                                "fuzziness" => "AUTO:4,6",
+                            ]
+                        ]
+                    ]
+                ],
+            ]
+        );
+
+        $this->result = [$response->getBody()->getContents(), 200];
+    }
+
+
+    /**
+     * @throws GuzzleException
+     */
+    public function search(): void
+    {
+        $response = $this->guzzle->post(
+            "{$this->hostPort}/{$this->indexName}/_search",
+            [
+                'headers' => $this->headers,
+                'json' => [
+                    "query" => [
+                        "match" => [
+                            "$this->documentName" => $this->constructSearchKey(
+                                $this->initials
+                            )
+                        ]
+                    ]
+                ],
+            ]
+        );
+
+        $this->result = [$response->getBody()->getContents(), 200];
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function wildCardSearch(): void
+    {
+        $response = $this->guzzle->post(
+            "{$this->hostPort}/{$this->indexName}/_search",
+            [
+                'headers' => $this->headers,
+                'json' => [
+                    "query" => [
+                        "wildcard" => [
+                            "$this->documentName" => [
+                                "value" => '*' . $this->constructSearchKey(
+                                        $this->initials
+                                    ) . '*'
+                            ]
+                        ]
+                    ]
+                ],
+            ]
+        );
+
+        $this->result = [$response->getBody()->getContents(), 200];
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function add(array $document): void
+    {
+        $response = $this->guzzle->post(
+            "{$this->hostPort}/{$this->indexName}/_doc",
+            [
+                'headers' => $this->headers,
+                'json' => $document
+            ]
+        );
+
+        $this->result = [$response->getBody()->getContents(), 200];
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function deleteDocument(string $documentId): void
+    {
+        $response = $this->guzzle->delete(
+            "{$this->hostPort}/{$this->indexName}/_doc/{$documentId}"
+        );
+
+        $this->result = [$response->getBody()->getContents(), 200];
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function getAll(): void
+    {
+        $response = $this->guzzle->get(
+            "{$this->hostPort}/{$this->indexName}/_search",
+            ['headers' => $this->headers]
+        );
+
+        $this->result = [$response->getBody()->getContents(), 200];
+
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function deleteIndex(): void
+    {
+
+        $response = $this->guzzle->delete(
+            "{$this->hostPort}/{$this->indexName}"
+        );
+
+        $this->result = [$response->getBody()->getContents(), 200];
+
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function getAllIndices(): void
+    {
+        $response = $this->guzzle->get(
+            "{$this->hostPort}/_cat/indices"
+        );
+
+        $this->result = [$response->getBody()->getContents(), 200];
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function countIndexDocuments(): void
+    {
+        $response = $this->guzzle->get(
+            "{$this->hostPort}/{$this->indexName}/_count"
+        );
+
+        $this->result = [$response->getBody()->getContents(), 200];
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function findAndDeleteByDocId(array $item): void
+    {
+        $response = $this->guzzle->get(
+            "{$this->hostPort}/{$this->indexName}/_search",
+            [
+                'json' => [
+                    'query' => [
+                        'term' => $item,
+                    ],
+                ],
+            ]
+        );
+
+        $blacklist = json_decode($response->getBody(), true);
+
+        if ($blacklist['hits']['total']['value'] === 0) {
+            Log::info(['no doc found by uid ', 'uid' => $item]);
+        } else {
+            foreach ($blacklist['hits']['hits'] as $hit) {
+                $documentId = $hit['_id'];
+                $this->guzzle->delete(
+                    "{$this->hostPort}/{$this->indexName}/_doc/{$documentId}"
+                );
+            }
+        }
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function deleteDocByTerrId(string $terrId): void
+    {
+        $response = $this->guzzle->get(
+            "{$this->hostPort}/{$this->indexName}/_search",
+            [
+                'json' => [
+                    'query' => [
+                        'term' => [
+                            'terr_id' => $terrId,
+                        ],
+                    ],
+                ],
+            ]
+        );
+
+        $blacklist = json_decode($response->getBody(), true);
+
+        if ($blacklist['hits']['total']['value'] === 0) {
+            $telegram = new Telegram(['body' => 'delete by terr id. no doc found in ES by terr id. ' . $terrId]);
+            $telegram->message();
+            $telegram->send();
+        } else {
+            foreach ($blacklist['hits']['hits'] as $hit) {
+                $documentId = $hit['_id'];
+                $this->guzzle->delete(
+                    "{$this->hostPort}/{$this->indexName}/_doc/{$documentId}"
+                );
+            }
+        }
+    }
+
+    /**
+     * it extracts the array of results
+     * exactSearchResults[0][_source']['initials']
+     *
+     * @throws GuzzleException
+     */
+    public function exactSearchAndSetResult(string $searchKey): void
+    {
+        $response = $this->guzzle->get(
+            "{$this->hostPort}/{$this->indexName}/_search",
+            [
+                'json' => [
+                    'query' => [
+                        'term' => [
+                            $this->searchField => $searchKey
+                        ],
+                    ],
+                ],
+            ]
+        );
+
+        $results = json_decode($response->getBody()->getContents(), true);
+
+        if (isset($results['hits']['hits']) && count($results['hits']['hits']) > 0) {
+            $this->result = $results['hits']['hits'];
+        }
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function updateClientInitialsByDocId(string $esDocId, string $newInitials): void
+    {
+        $this->guzzle->post(
+            "{$this->hostPort}/{$this->indexName}/_update/{$esDocId}",
+            [
+                'json' => [
+                    'script' => [
+                        'source' => 'ctx._source.initials = params.newInitials;',
+                        'lang' => 'painless',
+                        'params' => [
+                            'newInitials' => $newInitials,
+                        ],
+                    ],
+                ],
+            ]
+        );
+
+    }
+
+
+    /**
+     * @throws GuzzleException
+     */
+    public function updateWithId(array $document, int $id): void
+    {
+        $response = $this->guzzle->post(
+            "{$this->hostPort}/{$this->indexName}/_doc/{$id}",
+            [
+                'headers' => $this->headers,
+                'json' => $document
+            ]
+        );
+
+        $this->result = [$response->getBody()->getContents(), 200];
+    }
+
+    private function constructSearchKey(string $initials)
+    {
+    }
+
+}
